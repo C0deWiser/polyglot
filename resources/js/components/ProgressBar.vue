a9e8fd09c3b174fe2c0e58474ec936299388973c
Михаил Погребников <pm@101media.ru>
2021-11-16 14:10:08 +0300

    Compiled for prod

--- resources/js/components/ProgressBar.vue
+++ resources/js/components/ProgressBar.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="progress" style="height: 4px;">
-    <div class="progress-bar bg-success" :style="'width: ' + (stat.progress * 100) + '%'"
-         data-toggle="tooltip" data-placement="top" :title="stat.translated + ' translated string(s)'"></div>
-    <div class="progress-bar bg-warning" :style="'width: ' + (stat.problems * 100) + '%'"
-         data-toggle="tooltip" data-placement="top" :title="stat.fuzzy + ' fuzzy string(s)'"></div>
+  <div>
+    <div class="progress" style="height: 5px">
+      <div class="progress-bar bg-success" :style="'width: ' + (stat.progress * 100) + '%'"></div>
+      <div class="progress-bar bg-warning" :style="'width: ' + (stat.problems * 100) + '%'"></div>
+    </div>
+
   </div>
 </template>
 
