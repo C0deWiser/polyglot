4f5391495120aece10d2529b565bf9c141b4a2be
Михаил Погребников <pm@101media.ru>
2022-03-21 13:30:18 +0300

    Fix

--- src/Http/Middleware/AcceptLanguage.php
+++ src/Http/Middleware/AcceptLanguage.php
@@ -5,19 +5,12 @@ namespace Codewiser\Polyglot\Http\Middleware;
 use Closure;
 use Codewiser\Polyglot\Polyglot;
 use Illuminate\Http\Request;
-use Illuminate\Support\Arr;
 
 class AcceptLanguage
 {
     public function handle(Request $request, Closure $next)
     {
-        $locales = Polyglot::getLocales();
-
-        if (Arr::isAssoc($locales)) {
-            $locales = array_keys($locales);
-        }
-
-        app()->setLocale($request->getPreferredLanguage($locales));
+        app()->setLocale($request->getPreferredLanguage(Polyglot::getLocales()));
 
         return $next($request);
     }
